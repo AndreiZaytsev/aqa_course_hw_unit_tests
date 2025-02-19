fe9833f8f6be684ca197cc6817f13eefbe3027da
And <strawer@mail.ru>
2025-02-19 16:22:47 +0300

    HW-5 done

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -1,10 +1,18 @@
 /**
- * Создать строку с информацией о количестве гласных и согласных букв в слове.
- * Переменная `word` уже создана и содержит строку со словом.
- * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
+ * РЎРѕР·РґР°С‚СЊ СЃС‚СЂРѕРєСѓ СЃ РёРЅС„РѕСЂРјР°С†РёРµР№ Рѕ РєРѕР»РёС‡РµСЃС‚РІРµ РіР»Р°СЃРЅС‹С… Рё СЃРѕРіР»Р°СЃРЅС‹С… Р±СѓРєРІ РІ СЃР»РѕРІРµ.
+ * РџРµСЂРµРјРµРЅРЅР°СЏ `word` СѓР¶Рµ СЃРѕР·РґР°РЅР° Рё СЃРѕРґРµСЂР¶РёС‚ СЃС‚СЂРѕРєСѓ СЃРѕ СЃР»РѕРІРѕРј.
+ * РћР¶РёРґР°РµРјС‹Р№ СЂРµР·СѓР»СЊС‚Р°С‚ РґР»СЏ `hello`: "hello contains 2 vowels and 3 consonants".
  */
 const word = 'hello';
-
+let vowelsCount = 0;
+let consonantslsCount = 0;
 let vowelsAndConsonantsResult = '';
+for (let i = 0; i <= 4; i++) {
+  if (word[i] === 'e' || word[i] === 'o') vowelsCount++;
+  else {
+    consonantslsCount++;
+  }
+  vowelsAndConsonantsResult = `hello contains ${vowelsCount} vowels and ${consonantslsCount} consonants`;
+}
 
 export { vowelsAndConsonantsResult };
