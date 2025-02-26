7acc099770f5375ae2e9a709674eadfd08e0b061
And <strawer@mail.ru>
2025-02-26 13:16:04 +0300

    update 6 hw

--- homework-solutions/js/hw-6/task-2.js
+++ homework-solutions/js/hw-6/task-2.js
@@ -15,5 +15,25 @@
 
 let resultUnique;
 let resultNull;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+resultUnique = [];
+for (let i = 0; i < myPizzasT1.length; i++) {
+  if (myPizzasT1[i].toLowerCase() !== competitorPizzas[i].toLowerCase()) {
+    resultUnique.push(myPizzasT1[i]);
+  }
+}
+
+for (let i = 0; i < myPizzasT1.length; i++) {
+  if (myPizzasT2[i].toLowerCase() !== competitorPizzas[i].toLowerCase()) {
+    resultUnique.push(myPizzasT2[i]);
+  }
+}
+
+if (!resultUnique.length) {
+  resultNull = null;
+}
 
 export { resultNull, resultUnique };
